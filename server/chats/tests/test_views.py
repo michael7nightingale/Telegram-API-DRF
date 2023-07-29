b3092a279dbd8e533eeb0147e875c1091c5a5018
Michael Nightingale <122199262+michael7nightingale@users.noreply.github.com>
2023-07-29 12:52:17 +0500

    Update test_views.py

--- server/chats/tests/test_views.py
+++ server/chats/tests/test_views.py
@@ -1,16 +1,1 @@
-from rest_framework.test import APITestCase
-from django.urls import reverse
-
-from .fixtures import ChatFixture
-
-
-class TestChatView(ChatFixture, APITestCase):
-
-    def test_chat_create(self):
-        data = {
-            "account_id": self.account2.id,
-            "message": {
-                "text": "Hello, man"
-            }
-        }
 
