2a86f4f85fd68f513bcab6057a7aa106aa02b6ba
michael7nightingale <suslanchikmopl@gmail.com>
2023-07-29 11:08:40 +0500

    create chats models and serializers

---
+++ server/chats/models.py
@@ -0,0 +1,94 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+
+
+class ChatManager(models.Manager):
+
+    def create_chat(self, members: list):
+        chat = Chat()
+        chat.save()
+        chat.members.add(*members)
+        return chat
+
+    def add_message(self, chat, text: str, account, source):
+        if chat.members.filter(id=account.id).exists():
+            message = Message(
+                content_object=chat,
+                text=text,
+                account=account,
+                source=source
+            )
+            message.save()
+            chat.last_message = message
+            chat.save()
+        else:
+            return None
+
+    def all_account_chats(self, account):
+        return account.chats.all()
+
+
+class Chat(models.Model):
+    members = models.ManyToManyField(
+        "users.Account",
+        verbose_name="Members",
+        related_name="chats"
+    )
+    messages = GenericRelation("Message", object_id_field="id")
+    last_message = models.OneToOneField("Message", on_delete=models.SET_NULL, null=True, blank=True)
+
+    objects = ChatManager()
+
+
+class Group(models.Model):
+    members = models.ManyToManyField(
+        "users.Account",
+        verbose_name="Members",
+        related_name="group_chats"
+    )
+    is_public = models.BooleanField(default=True)
+    time_created = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        to="users.Account",
+        on_delete=models.CASCADE,
+        related_name="own_groups"
+    )
+
+    messages = GenericRelation("Message")
+
+    objects = ChatManager()
+
+
+class Message(models.Model):
+    content_object = GenericForeignKey(ct_field="content_type", fk_field="id")
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    account = models.ForeignKey("users.Account", on_delete=models.CASCADE)
+    text = models.TextField(max_length=2000, null=True)
+    source = models.ForeignKey(
+        to="self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    viewed = models.BooleanField(default=False)
+
+    time_send = models.DateTimeField("Time send", auto_created=True, auto_now=True)
+    time_update = models.DateTimeField("Time update", null=True, auto_now_add=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.text
+
+
+def media_upload_to(odj):
+    return f"uploads/{odj.id}"
+
+
+class MessageMedia(models.Model):
+    media = models.FileField("Media", upload_to=media_upload_to)
+    message = models.ForeignKey("Message", on_delete=models.CASCADE)
+
+    objects = models.Manager()
---
+++ server/chats/serializers.py
@@ -0,0 +1,83 @@
+from rest_framework import serializers
+
+from .models import Chat, Group, Message, MessageMedia
+from users.serializers import AccountDetailSerializer
+
+
+class MessageCreateSerializer(serializers.ModelSerializer):
+    account = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Message
+        fields = ("account", "text", "content_object", "source")
+
+
+class MessageDetailSerializer(serializers.ModelSerializer):
+    account = AccountDetailSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = (
+            "account",
+            "text",
+            "time_send",
+            "time_update",
+
+        )
+
+
+class MessageListSerializer(serializers.ModelSerializer):
+    account = AccountDetailSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = (
+            "account",
+            "text",
+            "source",
+
+        )
+
+
+class ChatCreateSerializer(serializers.ModelSerializer):
+    account = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    message = MessageCreateSerializer()
+    account_id = serializers.CharField()
+
+    class Meta:
+        model = Chat
+        fields = ("account_id", "message", "account")
+
+    def save(self, **kwargs):
+        kwargs.update(self.validated_data)
+        kwargs.pop("account_id")
+        chat = self.Meta.model.objects.create_chat(kwargs['members'])
+        Chat.objects.add_message(
+            chat=chat,
+            account=kwargs['message']['account'],
+            text=kwargs['message']['text'],
+            source=kwargs['message'].get('source')
+        )
+
+        return chat
+
+
+class ChatListSerializer(serializers.ModelSerializer):
+    messages = MessageListSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = ("id", "members", "messages")
+
+
+class ChatDetailSerializer(serializers.ModelSerializer):
+    messages = MessageDetailSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = (
+            "id",
+            "members",
+            "messages",
+
+        )
