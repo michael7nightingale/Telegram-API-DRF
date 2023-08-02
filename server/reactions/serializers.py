9901db639cd693df106609371057be3c0561eef7
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-02 10:21:01 +0500

    create reaction serializers

--- server/reactions/serializers.py
+++ server/reactions/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from rest_framework.exceptions import NotFound
 
+from chats.models import Message
+from users.serializers import AccountDetailSerializer
 from .models import Reaction, ReactionItem
 
 
@@ -15,7 +18,16 @@ class ReactionListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Reaction
-        fields = ("reaction_item", "account", "id")
+        fields = ("reaction_item", "account", "id", "message__id")
+
+
+class ReactionDetailSerializer(serializers.ModelSerializer):
+    reaction_item = ReactionItemListSerializer(read_only=True)
+    account = AccountDetailSerializer(read_only=True)
+
+    class Meta:
+        model = Reaction
+        fields = ("reaction_item", "account", "id", "message__id")
 
 
 class ReactionCreateSerializer(serializers.ModelSerializer):
@@ -26,3 +38,23 @@ class ReactionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reaction
         fields = ("account", "reaction_item_id", "message_id")
+
+    def save(self, **kwargs):
+        kwargs.update(self.validated_data)
+        reaction_item = ReactionItem.objects.get(id=kwargs['reaction_item_id'])
+        if reaction_item is None:
+            raise NotFound()
+        message = Message.objects.get(id=kwargs['message_id'])
+        if message is None:
+            raise NotFound()
+        Message.objects.check_member_permissions(message.content_object, kwargs['account'])
+        reaction, signal = Reaction.objects.create_reaction(
+            account=kwargs['account'],
+            reaction_item=reaction_item,
+            message=message
+        )
+        return reaction, signal
+
+
+class ReactionListRequestSerializer(serializers.Serializer):
+    message_id = serializers.CharField()
