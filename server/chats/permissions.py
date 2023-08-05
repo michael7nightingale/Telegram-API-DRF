a013845bea907aad60f34705103f4aa46bad0ea6
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-05 12:55:19 +0500

    create group permissions

--- server/chats/permissions.py
+++ server/chats/permissions.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import BasePermission
 
 
@@ -9,3 +10,14 @@ class IsChatMember(BasePermission):
 class IsGroupMember(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj in request.user.groups.all()
+
+
+def check_is_group_owner(account, group):
+    if account != group.owner:
+        raise PermissionDenied("You are not the owner.")
+
+
+def check_is_group_public(group):
+    if not group.is_public:
+        raise PermissionDenied("Group is not private.")
+
